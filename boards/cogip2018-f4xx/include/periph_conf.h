bbd26082412d108ee76a1bcff0045960b93bb271
Gilles DOFFE <g.doffe@gmail.com>
2018-04-08 01:08:18 +0200

    boards/cogip2018-f4xx: add new pwm_config
    Add a new pwm_config to manage a future motor driver.
    Timer TIM1, channel 3 is used.
    
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

--- boards/cogip2018-f4xx/include/periph_conf.h
+++ boards/cogip2018-f4xx/include/periph_conf.h
@@ -191,6 +191,16 @@ static const pwm_conf_t pwm_config[] = {
         .af       = GPIO_AF1,
         .bus      = APB1
     },
+    {
+        .dev      = TIM1,
+        .rcc_mask = RCC_APB2ENR_TIM1EN,
+        .chan     = { { .pin = GPIO_UNDEF,          .cc_chan = 0},
+                      { .pin = GPIO_UNDEF,          .cc_chan = 1},
+                      { .pin = GPIO_UNDEF,          .cc_chan = 2},
+                      { .pin = GPIO_PIN(PORT_A, 11),.cc_chan = 3} },
+        .af       = GPIO_AF1,
+        .bus      = APB2
+    },
 };
 
 #define PWM_NUMOF           (sizeof(pwm_config) / sizeof(pwm_config[0]))
